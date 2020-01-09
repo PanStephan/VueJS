0cba5e8e9a0fabb55f03ab469a13edf73c41b113
Stephan <stephan.startsev@gmail.com>
2020-01-09 21:52:12 +0300

    notify_app--pre-commit

---
+++ notify/src/store/index.js
@@ -0,0 +1,20 @@
+import Vue from 'vue'
+import Vuex from 'vuex'
+
+import notify from './notify'
+import errors from './errors'
+import loaders from './loaders'
+
+Vue.use(Vuex)
+
+export default new Vuex.Store({
+  modules: {
+    // state: {
+    //   error: null,
+    //   loaders: false
+    // },
+    notify,
+    // loaders,
+    // errors
+  }
+})
---
+++ notify/src/store/notify.js
@@ -0,0 +1,67 @@
+import loadMore from '../assets/js/loadMore.js'
+import axios from 'axios'
+
+export default {
+  state: {
+    messages: [],
+    messagesMain: [],
+  },
+  mutations: {
+    setMessage (state, payload) {
+      state.messages = payload
+    },
+    setMessageMain (state, payload) {
+      state.messagesMain = payload
+    },
+    loadMessages (state, payload) {
+      state.messagesMain = [...state.messagesMain, ...payload]
+    },
+  },
+  actions: {
+  async getNotify ({commit}) {
+    // dispatch('setLoading', true, { root: true })
+    await axios.get('https://tocode.ru/static/c/vue-pro/notifyApi.php')
+      .then(reseponse => {
+        let res = reseponse.data.notify,
+            messages = [],
+            messagesMain = []
+
+        // filter
+        for (let i = 0; i < res.length; i++) {
+          if (res[i].main) messagesMain.push(res[i])
+          else messages.push(res[i])
+        }   
+         
+        commit('setMessage', messages)
+        commit('setMessageMain', messagesMain)
+      })
+      .catch(error => {
+        // dispatch('setError', 'Error: Network Error', { root: true })
+      })
+      // .finally(() => (dispatch('setLoading', true), { root: true }))
+  },
+    setMessage ({commit}, payload) {
+      commit('setMessage', payload)
+    },
+    setMessageMain ({commit}, payload) {
+      commit('setMessageMain', payload)
+    },
+    loadMessages ({commit, getters}) {
+      let res = getters.getMessageFilter
+      commit('loadMessages', loadMore(res))
+    },
+  },
+  getters: {
+    getMessage (state) {
+      return state.messages
+    },
+    getMessageFilter (state) {
+      return state.messages.filter(mes => {
+        return mes.main === false
+      })
+    },
+    getMessageMain (state) {
+      return state.messagesMain
+    }
+  }
+}
